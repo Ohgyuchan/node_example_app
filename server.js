d1ce61470652ebbda09147586212a29b032379de
Ohgyuchan <gyu3637@gmail.com>
2022-08-17 15:36:41 +0900

    :construction: Update index.html

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.listen();
+app.listen(8080, function () {
+    console.log("listening on 8080");
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+app.get("/pet", function (req, res) {
+    res.send(`Hello, World`);
+});
